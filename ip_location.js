b6a151a0c5c7d8541a961818571d0a5cf33225d7
SalmanMoussaa <Salman.m.moussa@gmail.com>
2023-02-26 19:22:36 +0200

    adding location

--- ip_location.js
+++ ip_location.js
@@ -2,10 +2,10 @@ window.onload=function(){
     fetch('https://api.ipify.org?format=json')
     .then(response => response.json())
     .then(data => {
-      const ipAddress = data.ip;
-      let evenSum = 0;
-      for (let i = 0; i < ipAddress.length; i++) {
-        const digit = parseInt(ipAddress[i]);
+      var ipAddress = data.ip;
+      var evenSum = 0;
+      for (var i = 0; i < ipAddress.length; i++) {
+        var digit = parseInt(ipAddress[i]);
         if (!isNaN(digit) && digit % 2 === 0) {
           evenSum += digit;
         }
@@ -14,4 +14,29 @@ window.onload=function(){
       document.getElementById('evenSum').value = evenSum;
     })
     .catch(error => console.error(error));
+    
+    //location 
+    function showPosition(position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        const locationElement = document.getElementById("location");
+        locationElement.innerText = `Latitude: ${latitude}, Longitude: ${longitude}`;
+      }
+  
+      function showError(error) {
+        const locationElement = document.getElementById("location");
+        locationElement.innerText = `Error: ${error.message}`;
+      }
+  
+      function getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition, showError);
+        } else {
+          const locationElement = document.getElementById("location");
+          locationElement.innerText = "Geolocation is not supported by this browser.";
+        }
+
+      }
+      getLocation();
+      
 }
